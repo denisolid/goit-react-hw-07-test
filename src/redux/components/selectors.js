19484ce6b777331ab83625317929c2a7840225ac
denisolid <dkolisnichienko321@gmail.com>
2024-08-12 22:16:03 +0000

    selectors upd

--- src/redux/components/selectors.js
+++ src/redux/components/selectors.js
@@ -1,2 +1,4 @@
 export const selectContacts = (state) => state.contacts.items;
 export const selectNameFilter = (state) => state.filter.filter;
+export const selectIsError = (state) => state.contacts.isError;
+export const selectIsLoading = (state) => state.contacts.isLoading;
